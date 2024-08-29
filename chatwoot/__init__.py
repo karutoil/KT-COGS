bdf4915d7e39158b7cb080599b9e2a4172ed1915
karutoil <32721657+karutoil@users.noreply.github.com>
2024-08-29 18:54:57 -0400

    modified:   chatwoot/__init__.py

--- chatwoot/__init__.py
+++ chatwoot/__init__.py
@@ -1,10 +1,8 @@
-from redbot.core.bot import Red
-from redbot.core.utils import get_end_user_data_statement_or_raise
+from .chatwoot import chatwoot
+from redbot.core.utils import get_end_user_data_statement
 
-from .ChatwootCog import ChatwootCog
-
-__red_end_user_data_statement__ = get_end_user_data_statement_or_raise(__file__)
+__red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
 
-async def setup(bot: Red) -> None:
-    await bot.add_cog(ChatwootCog(bot))
+async def setup(bot):
+    await bot.add_cog(chatwoot(bot))
