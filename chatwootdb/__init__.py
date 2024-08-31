ac78a7c5d8a97b501a235afaa7f23ce07bfaeebf
karutoil <32721657+karutoil@users.noreply.github.com>
2024-08-30 23:01:08 -0400

    test

--- chatwootdb/__init__.py
+++ chatwootdb/__init__.py
@@ -2,6 +2,7 @@ from .chatwootdb import chatwootdb
 from redbot.core.utils import get_end_user_data_statement
 
 __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
+print("init")
 
 
 async def setup(bot):
